45a1337b234364e2deae37c50c218924afb5191f
Daniel20 <Daniel20@DANIEL-VARGAS20>
2024-10-17 19:03:23 -0500

    Se hace commit del microservicio de notificaciones

--- Ecommerce/Microservicio_Notificaciones/obj/Debug/net8.0/Microservicio_Notificaciones.AssemblyInfo.cs
+++ Ecommerce/Microservicio_Notificaciones/obj/Debug/net8.0/Microservicio_Notificaciones.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Microservicio_Usuarios")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Microservicio_Notificaciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192f14f354f6d410fa6be3c79df9d3993865f414")]
-[assembly: System.Reflection.AssemblyProductAttribute("Microservicio_Usuarios")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Microservicio_Usuarios")]
+[assembly: System.Reflection.AssemblyProductAttribute("Microservicio_Notificaciones")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Microservicio_Notificaciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
--- Ecommerce/Microservicio_Pagos/obj/Debug/net8.0/Microservicio_Pagos.AssemblyInfo.cs
+++ Ecommerce/Microservicio_Pagos/obj/Debug/net8.0/Microservicio_Pagos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microservicio_Pagos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c32ce69f07236067ed70c6de578049ec5f78e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+192f14f354f6d410fa6be3c79df9d3993865f414")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microservicio_Pagos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Microservicio_Pagos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
